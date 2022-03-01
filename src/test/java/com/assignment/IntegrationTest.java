ac447164ad7bb0b85b14fab37c0d6a07d2611cbc
Furkan Odeyen <furkanodeyen97@gmail.com>
2022-03-01 20:17:07 +0300

    Initial version of assignment generated by generator-jhipster@7.7.0

---
+++ src/test/java/com/assignment/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.assignment;
+
+import com.assignment.AssignmentApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = AssignmentApp.class)
+public @interface IntegrationTest {
+}
